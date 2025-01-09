e5ca9be8ff2dab8471edbb8c7eb19cb7bef72184
kathy6969 <164894096+kathy6969@users.noreply.github.com>
2025-01-10 02:37:31 +0700

    AnimtorPlayer
    player xong

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,17 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSeed = 5f;
-    [SerializeField] private float JumpForce = 15f;
+    [SerializeField] private float JumpForce = 5f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheck;
+    private Animator animator;
     private bool isGrounded;
     private Rigidbody2D rb;
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
     // Start is called before the first frame update
@@ -25,8 +27,9 @@ public class PlayerController : MonoBehaviour
     {
         HandleMoveMent();
         HanldeJump();
+        UpdateAmimation();
     }
-    private void HandleMoveMent()
+    private void HandleMoveMent() //di chuyển 
     {
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity=new Vector2 (moveInput*moveSeed, rb.velocity.y);
@@ -34,7 +37,7 @@ public class PlayerController : MonoBehaviour
         else if (moveInput < 0) transform.localScale = new Vector3(-1, 1, 1);
 
     }
-  private void HanldeJump()
+  private void HanldeJump() //check nhảy
     {
         if (Input.GetButtonDown("Jump")&&isGrounded)
         {
@@ -42,5 +45,12 @@ public class PlayerController : MonoBehaviour
 
         }
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+   private void UpdateAmimation()
+    {
+        bool isRunning = Mathf.Abs(rb.velocity.x) > 0.1f;
+        bool isJumping = !isGrounded;
+        animator.SetBool("IsRunning", isRunning);
+        animator.SetBool("IsJumping", isJumping);
     }
 }
